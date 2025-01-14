44a5abaf1660f0073cd2cee10d8c25745d5cad1a
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2025-01-14 20:49:38 +0900

    feat: 체크 표시

--- src/components/challenge/baek/item.tsx
+++ src/components/challenge/baek/item.tsx
@@ -1,14 +1,34 @@
+'use client';
+
+import { useState } from 'react';
+import { Check } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+
 type BaekChallengeItemProps = {
   day: number;
 };
 
 const BaekChallengeItem = (props: BaekChallengeItemProps) => {
   const { day } = props;
+  const [clicked, setIsClicked] = useState(false);
   const borderR = day % 10 !== 0 ? 'border-r' : '';
+
   return (
     <li
-      className={`h-14 py-0.5 px-1 border-r-primary ${borderR} border-b-primary border-b`}>
-      {day}
+      className={`relative h-14 py-0.5 px-1 border-r-primary ${borderR} border-b-primary border-b`}>
+      <Button
+        className='w-full h-full justify-start px-0 items-start py-0 hover:bg-accent-transparent'
+        variant='ghost'
+        onClick={() => setIsClicked((prev) => !prev)}>
+        {day}
+      </Button>
+      {clicked && (
+        <Check
+          className='absolute top-1.5 left-2 pointer-events-none'
+          size={48}
+        />
+      )}
     </li>
   );
 };
